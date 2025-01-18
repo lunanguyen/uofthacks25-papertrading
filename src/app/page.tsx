bf8bcc6eaffcab0af3169d221b63770b804cd171
monmon <monmonart12@gmail.com>
2025-01-17 22:13:49 -0500

    exp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,17 @@ export default function Home() {
         Trading</h1>
 
         <h1> with <button className="underline decoration-solid">Paper Trading</button>, your parents won't disown you !</h1>
+        
+        <div className="font-extrabold text-3xl md:text-4xl [text-wrap:balance] bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 to-50% to-slate-200">Trusted by the most innovative minds in <span className="text-indigo-500 inline-flex flex-col h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.4xl)*theme(lineHeight.tight))] overflow-hidden">
+            <ul className="block animate-text-slide-5 text-left leading-tight [&_li]:block">
+                <li>Finance</li>
+                <li>Tech</li>
+                <li>AI</li>
+                <li>Crypto</li>
+                <li>eCommerce</li>
+                <li aria-hidden="true">Finance</li>
+            </ul>
+        </span></div>
         <img src = "https://www.pngall.com/wp-content/uploads/8/Stock-Trading-PNG-High-Quality-Image.png" width="60%"
         className="mt-10"/>
       </div>
